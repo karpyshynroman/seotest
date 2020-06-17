cc6681150a231a5f9d88f9b6e4c890c91b64773e
Karpyshyn Roman <karpyshyn.roman@gmail.com>
2020-06-17 11:07:10 +0300

    d

--- src/App.js
+++ src/App.js
@@ -2,18 +2,18 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import PostList from "./components/PostList";
-import {BrowserRouter, Route, Router, Switch} from "react-router-dom";
+import {Router, Route, Switch, HashRouter} from "react-router-dom";
 import PostDetail from "./components/PostDetail";
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <HashRouter>
         <Switch>
           <Route exact path='/' component={PostList}/>
           <Route path='/:id' component={PostDetail}/>
         </Switch>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
