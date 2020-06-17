7b4afd567a59d2c4db26945cd1c7f95d807f0442
Karpyshyn Roman <karpyshyn.roman@gmail.com>
2020-06-17 10:36:00 +0300

    Updates

---
+++ precache-manifest.49625e7fa27b99cf00111960295178a4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2407d94a11f09e8a72730b41c716eed",
+    "url": "/seotest/index.html"
+  },
+  {
+    "revision": "eaf5ee32eb66ec6f9a20",
+    "url": "/seotest/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "eb702713bdf2f3733a35",
+    "url": "/seotest/static/js/2.2e60239e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/seotest/static/js/2.2e60239e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eaf5ee32eb66ec6f9a20",
+    "url": "/seotest/static/js/main.bcc571b2.chunk.js"
+  },
+  {
+    "revision": "10bf1c2898f4168dc796",
+    "url": "/seotest/static/js/runtime-main.80e95f06.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/seotest/static/media/logo.5d5d9eef.svg"
+  }
+]);
